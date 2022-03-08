c9f4806ab88817a6d8d176ade0d1a267933cb80e
Devi1k <570541309@qq.com>
2022-03-08 21:41:38 +0800

    增加ai wrapper 测试ws

--- main_test.py
+++ main_test.py
@@ -5,12 +5,10 @@ from multiprocessing import Pipe, Process
 import gensim
 import websockets
 
+from ai_wrapper import get_answer
 from conf.config import get_config
 from gov.running_steward import simulation_epoch
 from message_sender import messageSender
-
-pipes_dict = {}
-end_flag = "END"
 
 
 async def main_logic(para, mod):
@@ -19,9 +17,16 @@ async def main_logic(para, mod):
         '''
     user_json = json.loads(response_json)
 
+    global first_utterance
+    global service_name
+    global conv_id
     while True:
         async with websockets.connect('wss://asueeer.com/ws?mock_login=123') as websocket:
-            # await websocket.send('{"type":0, "msg": "123"}')  # 测试接口
+            data = {"type": 101, "msg": {"conv_id": "1475055770457346048", "content": {
+                "judge": True, "text": '护照丢了怎么办'
+            }, }}
+            s = json.dumps(data, ensure_ascii=False)
+            await websocket.send(s)  # 测试接口
             response = await websocket.recv()
 
             # for res in test_text:
@@ -39,6 +44,8 @@ async def main_logic(para, mod):
                 Process(target=simulation_epoch, args=((user_pipe[1], response_pipe[0]), para, mod)).start()
                 # Process(target=messageSender, args=(conv_id, end_flag, response_pipe[1], user_pipe[0])).start()
             else:
+                if first_utterance == "":
+                    first_utterance = msg['content']  # TODO:? 是放在这里还是上面
                 user_pipe, response_pipe = pipes_dict[conv_id]
                 user_text = msg['content']
                 # 初始化会话后 向模型发送判断以及描述（包括此后的判断以及补充描述
@@ -53,20 +60,29 @@ async def main_logic(para, mod):
                 """
                 # 没结束 继续输入
                 if recv['end_flag'] is not True:
+                    # todo: message_sender
                     msg = recv['service']
+                    print(msg)
                     messageSender(conv_id, msg)
                 # 结束关闭管道
                 else:
                     user_pipe[0].close()
                     response_pipe[1].close()
-                    # todo: service_name
                     service_name = recv['service']
-                    print(service_name)
                     break
 
 
 if __name__ == '__main__':
+    end_flag = "END"
+    pipes_dict = {}
+    first_utterance, service_name = "", ""
     model = gensim.models.Word2Vec.load('data/wb.text.model')
     config_file = './conf/settings.yaml'
     parameter = get_config(config_file)
     asyncio.get_event_loop().run_until_complete(main_logic(parameter, model))
+    print("first_utterance: ", first_utterance)
+    print("service_name: ", service_name)
+    answer = get_answer(first_utterance, service_name)
+    print(answer)
+    # todo: 将字符串answer发给前端
+    messageSender(conv_id, answer)
