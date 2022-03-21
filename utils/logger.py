f8aec7877cb8e10f23165dd58e79f451f39993c7
Devi1k <570541309@qq.com>
2022-03-21 20:19:51 +0800

    add timeRotateLogger

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,23 @@
 import logging
+import os
+import re
+from logging.handlers import TimedRotatingFileHandler
 
 
 class Logger:
     def __init__(self):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.log_path = os.getcwd() + '/log/asr'
+        self.log_fmt = '%(asctime)s\tFile \"%(filename)s\",line %(lineno)s\t%(levelname)s: %(message)s'
+        self.formatter = logging.Formatter(self.log_fmt)
         self.logger = logging.getLogger("Diagnose")
 
     def getLogger(self):
-        handler1 = logging.FileHandler("base" + ".log")
-        handler1.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s|%(name)-12s+ %(levelname)-8s++%(message)s')
-        handler1.setFormatter(formatter)
-        self.logger.addHandler(handler1)
+        self.logger.setLevel(logging.INFO)
+        self.logger.suffix = "%Y-%m-%d_%H-%M.log"
+        self.logger.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}.log$")
+        log_file_handler = TimedRotatingFileHandler(filename=self.log_path, when="D", interval=1, backupCount=7)
+        log_file_handler.setFormatter(self.formatter)
+
+        self.logger.addHandler(log_file_handler)
         return self.logger
